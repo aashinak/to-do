62389d0be20c8b14a2a8ed3c142586112d048c87
AASHIN A K <88496253+aashinak@users.noreply.github.com>
2023-05-15 20:15:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const button= document.getElementById('btn')
+const textBox= document.getElementById('input-val')
+const list =document.getElementById('lists')
+
+
+button.addEventListener('click', btnClick)
+addEventListener('keypress', (e)=>{
+    if (e.key === 'Enter') {
+        btnClick()
+      }
+})
+function btnClick(){
+    var inputText= textBox.value
+    if(inputText ===""){
+        alert('You have to enter something')
+    }else{
+        
+        const listBox= document.createElement("li")
+        listBox.innerText=textBox.value
+        list.appendChild(listBox)
+        const span =document.createElement("img")
+        span.src="/image/close.png"
+        listBox.appendChild(span)
+        textBox.value=""
+        
+        
+    }
+}
+list.addEventListener('click', (e)=>{
+   
+
+    if(e.target.tagName ==='LI'){
+        e.target.classList.toggle('check')
+    }else if(e.target.tagName ==="IMG"){
+        e.target.parentElement.remove()
+    }
+
+    
+})
+
+
+
+
